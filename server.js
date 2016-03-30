56e406502b2875c1644de8d8decd2bbb51c49a4f
Jacky Tang <jackytck@gmail.com>
2016-03-31 01:07:31 +0800

    + implement timestamp microservice

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
-'use strict';
+import express from 'express'
+import routes from './app/routes/index.js'
+import mongoose from 'mongoose'
+import passport from 'passport'
+import session from 'express-session'
+import appPassport from './app/config/passport'
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+const app = express()
+appPassport(passport)
+mongoose.connect(process.env.MONGO_URI)
 
-var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
-
-mongoose.connect(process.env.MONGO_URI);
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'))
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use('/common', express.static(process.cwd() + '/app/common'))
 
 app.use(session({
 	secret: 'secretClementine',
 	resave: false,
 	saveUninitialized: true
-}));
+}))
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-routes(app, passport);
+routes(app, passport)
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
-});
+	console.log(`Node.js listening on port ${port}...`)
+})
